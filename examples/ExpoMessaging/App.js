76b2fd0697182cf8fe2bd5e5b1c3ff356a02da3e
charles francis <charlsefrancis112@gmail.com>
2021-02-01 16:30:58 +0800

    Update App.js

--- examples/ExpoMessaging/App.js
+++ examples/ExpoMessaging/App.js
@@ -70,6 +70,54 @@ const streami18n = new Streami18n({
   language: 'en',
 });
 
+
+import { Text, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import { AppearanceProvider, useColorScheme } from 'react-native-appearance';
+
+export default function AppContainer() {
+  return (
+    <AppearanceProvider>
+      <App />
+    </AppearanceProvider>
+  );
+}
+
+function App() {
+  const colorScheme = useColorScheme();
+
+  const themeStatusBarStyle = colorScheme === 'light' ? 'dark-content' : 'light-content';
+  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+
+  return (
+    <SafeAreaView style={[styles.container, themeContainerStyle]}>
+      <StatusBar barStyle={themeStatusBarStyle} />
+      <Text style={[styles.text, themeTextStyle]}>Color scheme: {colorScheme}</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lightContainer: {
+    backgroundColor: '#D0D0C0',
+  },
+  darkContainer: {
+    backgroundColor: '#242C40',
+  },
+  lightThemeText: {
+    color: '#242C40',
+  },
+  darkThemeText: {
+    color: '#D0D0C0',
+  },
+});
+
 const ChannelListScreen = React.memo(({ navigation }) => {
   const { setChannel } = useContext(AppContext);
   return (
